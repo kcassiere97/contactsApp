db3b4f26401a69f4a255181f12e4eb429204b0e8
KizarC <kcassiere97@gmail.com>
2022-01-06 20:01:22 -0800

    docker file update, contact.py update

--- contacts.py
+++ contacts.py
@@ -1,9 +1,6 @@
 import mysql.connector
-import Flask as flask
-
-
-
-app = flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def contacts():
@@ -20,13 +17,21 @@ def contacts():
     else:
         print("Connected Successfully")
 
+    fn = "get fname"
+    ln = "get lname"
+    email = "get email"
+    phone = "get number"
     #Create Database
     mycursor = db.cursor()
     mycursor.execute("CREATE DATABASE contactsDb")
     mycursor.execute("CREATE TABLE contact (fname VARCHAR(20), lname VARCHAR(20), phoneNumber VARCHAR(10), email VARCHAR(20))")
+    mycursor.execute("INSERT INTO contactDb (fname,lname,phoneNumber,email) VALUES(?,?,?,?)",(fn,ln,email,phone))
+    mycursor.commit()
+    print("record added")
     db.close()
     
     return 'Success!'
     
-
+if __name__ == '__main__':
+   app.run(debug = True)
 
